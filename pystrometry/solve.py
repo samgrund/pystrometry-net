8d269c1f126cd3ead6978bfd7c9683365ab3c5fa
Samuel <samuel_grund@hotmail.com>
2021-11-05 08:16:17 +0100

    Change folder structure & gitignore

--- pystrometry/solve.py
+++ pystrometry/solve.py
@@ -1,16 +1,17 @@
 from astropy.io import fits
 import subprocess
 import time
-import tempfile
 import os
 import warnings
 
 
 def solveField(hdul,RA,DEC,radius=2,cmdstring="--no-plots --downsample 4  --scale-units degwidth --scale-low 0.4 --scale-high 0.6 --crpix-center"):
-    dn = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    dn = os.path.dirname(os.path.realpath(__file__))
     tempfits = os.path.join(dn,'tmp/temp.fits')
     wcsfits  = os.path.join(dn,'tmp/wcsfits.fits')
-    hdul.writeto('./tmp/temp.fits',overwrite=True)
+    solvedfile = os.path.join(dn,'tmp/temp.solved')
+    print(tempfits)
+    hdul.writeto(str(tempfits),overwrite=True)
     cmds = f'solve-field {tempfits} --overwrite --new-fits {wcsfits} --ra {RA} --dec {DEC} --radius {radius}'
     cmds = cmds.split()
     cmds += cmdstring.split()
@@ -20,7 +21,7 @@ def solveField(hdul,RA,DEC,radius=2,cmdstring="--no-plots --downsample 4  --scal
     stdout, stderr = process.communicate()
     end = time.time()
 
-    if os.path.exists('./tmp/temp.solved'):
+    if os.path.exists(solvedfile):
         #print(f'solved field in {round(end-start,2)} seconds!')
         newheader = fits.getheader(wcsfits)
         return newheader
